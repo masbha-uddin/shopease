da3fb0bf60c43993bc7c18e46b254740e8711c42
masbha-uddin <masbha192@gmail.com>
2026-03-19 12:54:45 -0600

    first-project

---
+++ script.js
@@ -0,0 +1,6 @@
+// let search = document.getElementById('search');
+// let searchSubmit = document.getElementById('search_submit');
+
+// searchSubmit.addEventListener('click', function(){
+//     search.clear = '';
+// })
